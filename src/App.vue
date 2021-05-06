ef22a74c315d47f0e92bc2fc6878a88f0c16b0d3
mubens <zhuxiaofengzj@qq.com>
2021-05-06 18:48:53 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ToRef />
+  <TpVideo />
+  <TpVideoController />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
